2b7b0cc7c965b7f66351377c8cb4ff771124c218
Izza Ihsan Fathony <izzafathony27@gmail.com>
2023-06-06 22:41:03 +0700

    dummy product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Manufacturer;
 use App\Models\Product;
+use App\Models\Type;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
 
@@ -13,54 +15,61 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $manufacturers = Manufacturer::with('id', 'name')->get();
+        $products = Product::with('type', 'manufacturer')->get();
+        $types = Type::with('id', 'name')->get();
+
+        $context = [
+            'manufacturers' => $manufacturers,
+            'products' => $products,
+            'types' => $types
+        ];
+
+        return view('admin.product', $context);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(Request $request)
     {
-        //
-    }
+        Product::create([
+            'name' => $request->name,
+            'type' => $request->type,
+            'manufacturer' => $request->manufacturer,
+            'base_price' => $request->base_price,
+            'sell_price' => $request->sell_price,
+            'stock' => $request->stock,
+        ]);
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreProductRequest $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Product $product)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Product $product)
-    {
-        //
+        return redirect()->route('admin.product.index');
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateProductRequest $request, Product $product)
+    public function update(UpdateProductRequest $request, Product $product, $id)
     {
-        //
+        Product::where('id', $id)->update([
+            'name' => $request->name,
+            'type' => $request->type,
+            'manufacturer' => $request->manufacturer,
+            'base_price' => $request->base_price,
+            'sell_price' => $request->sell_price,
+            'stock' => $request->stock,
+        ]);
+
+        return redirect()->route('admin.product.index');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Product $product)
+    public function destroy(Product $product, $id)
     {
-        //
+        $product = Product::find($id);
+        $product->delete();
+
+        return redirect()->route('admin.product.index');
     }
 }
