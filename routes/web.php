d909a74fe588cd1f409c383f70b90ba17bdbddba
Izza Ihsan Fathony <izzafathony27@gmail.com>
2023-07-02 18:57:21 +0700

    add manty route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ManTyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -9,19 +11,28 @@ use Illuminate\Support\Facades\Route;
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| be assigned to the 'web' middleware group. Make something great!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
+})->name('index');
+
+Route::prefix('product')->group(function() {
+    Route::controller(ProductController::class)->group(function() {
+        Route::get('/', 'index')->name('product.index');
+        Route::post('/create', 'create')->name('product.create');
+        Route::post('/update/{id}', 'update')->name('product.update');
+        Route::delete('/delete/{id}', 'delete')->name('product.delete');
+    });
 });
 
-Route::prefix("product")->group(function() {
-    Route::controller(ProductController::class)->group(function() {
-        Route::get('/', 'index')->name('admin.product.index');
-        Route::post('/create', 'create')->name('admin.product.create');
-        Route::post('/update/{id}', 'update')->name('admin.product.update');
-        Route::delete('delete/{id}', 'delete')->name('admin.product.delete');
+Route::prefix('type-manufacturer')->group(function() {
+    Route::controller(ManTyController::class)->group(function() {
+        Route::get('/', 'index')->name('manty.index');
+        Route::post('/create', 'create')->name('manty.create');
+        Route::post('/update/{id}', 'update')->name('manty.update');
+        Route::delete('/delete/{id}', 'delete')->name('manty.delete');
     });
 });
